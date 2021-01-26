760872f08b91e46f3cc34ed0ac747a6a72830d65
dc2586515515 <49047922+dc2586515515@users.noreply.github.com>
2021-01-26 16:22:26 +0800

    HttpClient Post请求

--- 09-爬虫/9.4-网络爬虫 HttpClient Jsoup WebMagic/itcast-crawler-first/src/main/java/cn/itcast/crawler/test/HttpPostTest.java
+++ 09-爬虫/9.4-网络爬虫 HttpClient Jsoup WebMagic/itcast-crawler-first/src/main/java/cn/itcast/crawler/test/HttpPostTest.java
@@ -1,7 +1,7 @@
 package cn.itcast.crawler.test;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -14,25 +14,25 @@ import java.io.IOException;
  * @Author DC
  * @Date 2021-01-26
  */
-public class HttpGetParamTest {
+public class HttpPostTest {
     public static void main(String[] args) throws Exception {
         // 创建HttpClient对象
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
         // 设置请求地址是: http://yun.itheima.com/search?keys=Java
         // 创建URIBuilder
-        URIBuilder uriBuilder = new URIBuilder("https://www.baidu.com/s");
+        URIBuilder uriBuilder = new URIBuilder("http://yun.itheima.com/search?keys=Java");
         // 设置参数
-        uriBuilder.setParameter("word", "NBA");
-        // 创建HttpGet对象, 设置url访问地址
-        HttpGet httpGet = new HttpGet(uriBuilder.build());
+        uriBuilder.setParameter("keys", "Java");
+        // 创建HttpPost对象, 设置url访问地址
+        HttpPost httpPost = new HttpPost(uriBuilder.build());
 
-        System.out.println("发起请求的信息: " + httpGet);
+        System.out.println("发起请求的信息: " + httpPost);
 
         CloseableHttpResponse response = null;
         try {
             // 使用 HttpClient 发起请求, 获取 response
-            response = httpClient.execute(httpGet);
+            response = httpClient.execute(httpPost);
             // 解析响应
             if (response.getStatusLine().getStatusCode() == 200) {
                 String content = EntityUtils.toString(response.getEntity(), "utf8");
