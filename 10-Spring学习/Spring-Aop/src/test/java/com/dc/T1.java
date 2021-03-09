17f9ef39368a02b79a506041b3814836db42f0e5
dc2586515515 <49047922+dc2586515515@users.noreply.github.com>
2021-03-09 10:16:20 +0800

    Spring5 AOP顺序

--- 10-Spring学习/Spring-Aop/src/test/java/com/dc/T1.java
+++ 10-Spring学习/Spring-Aop/src/test/java/com/dc/T1.java
@@ -1,13 +1,14 @@
 package com.dc;
 
 import com.dc.service.CalcService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+// import org.junit.Test;   // SpringBoot 1.*需要 ，SPringBoot 2.*不再需要
+// import org.junit.runner.RunWith;   // SpringBoot 1.*需要 ，SPringBoot 2.*不再需要
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.SpringVersion;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringRunner;  // SpringBoot 1.*需要 ，SPringBoot 2.*不再需要
 
 /**
  * @Description
@@ -15,13 +16,21 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @Date 2021-03-09
  */
 @SpringBootTest
-@RunWith(SpringRunner.class)
+// @RunWith(SpringRunner.class)
 public class T1 {
     @Autowired
     private CalcService calcService;
 
-    @Test
+    /*@Test
     public void testSpring4() {
+        System.out.println("spring版本：" + SpringVersion.getVersion() + "\t" + "SpringBoot版本：" + SpringBootVersion.getVersion());
+        System.out.println();
+        // calcService.div(10, 2);
+        calcService.div(10, 0);
+    }*/
+
+    @Test
+    public void testSpring5() {
         System.out.println("spring版本：" + SpringVersion.getVersion() + "\t" + "SpringBoot版本：" + SpringBootVersion.getVersion());
         System.out.println();
         // calcService.div(10, 2);
