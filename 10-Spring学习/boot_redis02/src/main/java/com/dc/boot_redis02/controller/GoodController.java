c5549db01b92b804410734b273c0202e17a73b46
dc2586515515 <49047922+dc2586515515@users.noreply.github.com>
2021-03-10 14:30:35 +0800

    加分布式锁

--- 10-Spring学习/boot_redis02/src/main/java/com/dc/boot_redis02/controller/GoodController.java
+++ 10-Spring学习/boot_redis02/src/main/java/com/dc/boot_redis02/controller/GoodController.java
@@ -6,6 +6,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -17,6 +18,8 @@ import java.util.concurrent.locks.ReentrantLock;
 @RestController
 public class GoodController {
 
+    public static final String REDIS_LOCK_KEY = "lockredis";
+
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
@@ -25,22 +28,24 @@ public class GoodController {
 
     @GetMapping("/buy_goods")
     public String buy_Goods() {
+        String value = UUID.randomUUID().toString() + Thread.currentThread().getName();
 
-        synchronized (this) {
-            String result = stringRedisTemplate.opsForValue().get("goods:001");
-            int goodsNumber = result == null ? 0 : Integer.parseInt(result);
-
-            if (goodsNumber > 0) {
-                int realNumber = goodsNumber - 1;
-                stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
-                System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
-                return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
-            } else {
-                System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
-            }
-            return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK_KEY, value);
+        if (!flag) {
+            return "抢锁失败，┭┮﹏┭┮";
         }
+        String result = stringRedisTemplate.opsForValue().get("goods:001");
+        int goodsNumber = result == null ? 0 : Integer.parseInt(result);
 
+        if (goodsNumber > 0) {
+            int realNumber = goodsNumber - 1;
+            stringRedisTemplate.opsForValue().set("goods:001", realNumber + "");
+            System.out.println("你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort);
+            return "你已经成功秒杀商品，此时还剩余：" + realNumber + "件" + "\t 服务器端口: " + serverPort;
+        } else {
+            System.out.println("商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort);
+        }
+        return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
     }
 
 }
