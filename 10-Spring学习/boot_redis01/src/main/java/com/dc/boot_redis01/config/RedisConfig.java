bd72e34cc5017e8c46567ab4c5fd6fa86f84d389
dc2586515515 <49047922+dc2586515515@users.noreply.github.com>
2021-03-10 10:53:33 +0800

    boot + redis

---
+++ 10-Spring学习/boot_redis01/src/main/java/com/dc/boot_redis01/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.dc.boot_redis01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * @Description
+ * @Author DC
+ * @Date 2021-03-10
+ */
+@Configuration
+public class RedisConfig {
+    /**
+     * 保证不是序列化后的乱码配置
+     */
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
