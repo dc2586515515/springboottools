dbcf39cf7da6559d8d037987f4751b51d574bf9a
dc2586515515 <49047922+dc2586515515@users.noreply.github.com>
2021-03-10 15:49:17 +0800

    finally块的判断+del删除操作不是原子性的,推荐使用lua脚本保证原子性，但是如果不让用lua脚本的话，可以用redis事务

--- 10-Spring学习/boot_redis01/src/main/java/com/dc/boot_redis01/controller/GoodController.java
+++ 10-Spring学习/boot_redis01/src/main/java/com/dc/boot_redis01/controller/GoodController.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -55,9 +56,26 @@ public class GoodController {
             return "商品已经售罄/活动结束/调用超时，欢迎下次光临" + "\t 服务器端口: " + serverPort;
         } finally {
             // 如果锁超时时间设置10s，A线程执行了12s，在第10s时，redis已经将A的锁删除，同时B线程获得锁，第12S时，A线程执行完毕，删除锁，但此时B还在执行，A删除的是B的锁，误删了别人的锁，因此，需要判断Value是否等于自己的锁的值，才能删除
-            if (stringRedisTemplate.opsForValue().get(REDIS_LOCK_KEY).equals(value)) {
-                // 释放锁
-                stringRedisTemplate.delete(REDIS_LOCK_KEY);
+            // if (stringRedisTemplate.opsForValue().get(REDIS_LOCK_KEY).equals(value)) {
+            //     // 释放锁
+            //     stringRedisTemplate.delete(REDIS_LOCK_KEY);
+            // }
+
+            // 上述 判断+del删除操作 不是原子性的，推荐使用lua脚本保证原子性，但是如果不让用lua脚本的话，可以用redis事务
+            while (true) {
+                stringRedisTemplate.watch(REDIS_LOCK_KEY); //加事务，乐观锁
+                if (value.equalsIgnoreCase(stringRedisTemplate.opsForValue().get(REDIS_LOCK_KEY))) {
+                    stringRedisTemplate.setEnableTransactionSupport(true);
+                    stringRedisTemplate.multi();//开始事务
+                    stringRedisTemplate.delete(REDIS_LOCK_KEY);
+                    List<Object> list = stringRedisTemplate.exec();
+                    if (list == null) {  //如果等于null，就是没有删掉，删除失败，再回去while循环那再重新执行删除
+                        continue;
+                    }
+                }
+                //如果删除成功，释放监控器，并且breank跳出当前循环
+                stringRedisTemplate.unwatch();
+                break;
             }
         }
     }
