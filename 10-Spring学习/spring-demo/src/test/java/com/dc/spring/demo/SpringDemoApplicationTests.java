d958e583cea412a228a8f5fecf17c713e031caf0
dc2586515515 <49047922+dc2586515515@users.noreply.github.com>
2021-03-25 10:30:19 +0800

    SPI

---
+++ 10-Spring学习/spring-demo/src/test/java/com/dc/spring/demo/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
