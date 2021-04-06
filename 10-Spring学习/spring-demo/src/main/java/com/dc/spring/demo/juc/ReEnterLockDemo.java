6d4e3be51c2136e4e14e609e2d20600d7437f007
2586515115@qq.com <2586515115@qq.com>
2021-04-06 16:54:40 +0800

    synchronized 可重入锁。

---
+++ 10-Spring学习/spring-demo/src/main/java/com/dc/spring/demo/juc/ReEnterLockDemo.java
@@ -0,0 +1,29 @@
+package com.dc.spring.demo.juc;
+
+
+/**
+ * 可重入锁:可重复可递归调用的锁，在外层使用锁之后，在内层仍然可以使用，并且不发生死锁，这样的锁就叫做可重入锁。
+ * 在一个synchronized修饰的方法或代码块的内部
+ * 调用本类的其他synchronized修饰的方法或代码块时，是永远可以得到锁的
+ */
+public class ReEnterLockDemo {
+    static Object objectLockA = new Object();
+
+    static void m1() {
+        new Thread(() -> {
+            synchronized (objectLockA) {
+                System.out.println(Thread.currentThread().getName() + "\t" + "外层调用");
+                synchronized (objectLockA) {
+                    System.out.println(Thread.currentThread().getName() + "\t" + "中层调用");
+                    synchronized (objectLockA) {
+                        System.out.println(Thread.currentThread().getName() + "\t" + "内层调用");
+                    }
+                }
+            }
+        }, "t1").start();
+    }
+
+    public static void main(String[] args) {
+        m1();
+    }
+}
