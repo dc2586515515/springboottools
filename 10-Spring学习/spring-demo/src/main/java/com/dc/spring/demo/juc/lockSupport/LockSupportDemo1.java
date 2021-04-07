66c7121ada995f43fd9abc89b09ec92fd87ab301
2586515115@qq.com <2586515115@qq.com>
2021-04-07 17:15:50 +0800

    LockSupport

--- 10-Spring学习/spring-demo/src/main/java/com/dc/spring/demo/juc/lockSupport/LockSupportDemo1.java
+++ 10-Spring学习/spring-demo/src/main/java/com/dc/spring/demo/juc/lockSupport/LockSupportDemo1.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -27,6 +28,41 @@ public class LockSupportDemo1 {
     static Condition condition = lock.newCondition();
 
     public static void main(String[] args) {
+
+        lockSupport();
+    }
+
+    /**
+     * LockSupport：俗称 锁中断
+     * 以前的两种方式：
+     * 1.以前的等待唤醒通知机制必须synchronized里面有一个wait和notify
+     * 2.lock里面有await和signal
+     * 这上面这两个都必须要持有锁才能干，
+     * LockSupport它的解决的痛点
+     * 1。LockSupport不用持有锁块，不用加锁，程序性能好，
+     * 2。先后顺序，不容易导致卡死
+     */
+    private static void lockSupport() {
+        Thread a = new Thread(() -> {
+            // 暂停几秒钟线程
+            // try { TimeUnit.SECONDS.sleep(3L);} catch (InterruptedException e) { e.printStackTrace(); }
+            System.out.println(Thread.currentThread().getName() + "\t" + "========com in" + System.currentTimeMillis());
+            LockSupport.park(); // 被阻塞。。。。等待通知放行，他要通过需要许可证
+            System.out.println(Thread.currentThread().getName() + "\t" + "被唤醒" + System.currentTimeMillis());
+        }, "A");
+        a.start();
+
+        Thread b = new Thread(() -> {
+            LockSupport.unpark(a);
+            System.out.println(Thread.currentThread().getName() + "\t" + "通知了");
+        }, "b");
+        b.start();
+    }
+
+    /**
+     * lock await signal 组合使用
+     */
+    private static void lockAwaitSignal() {
         new Thread(() -> {
             // 暂停几秒钟线程
             // try { TimeUnit.SECONDS.sleep(3L);} catch (InterruptedException e) { e.printStackTrace(); } // 先await后signal才OK，顺序不能反
